045f46fdccc9ba08196ec36d09b287cc089c4520
Jens Krause <jens@crazy-factory.com>
2016-10-26 10:07:23 +0700

    flatten descriptions

--- config/api.js
+++ config/api.js
@@ -4,18 +4,8 @@ var Api_response =
     "db_id": "12345",
     "propertyFormula": "+[gauge:1.6]\n+[length:38]\n+[ball:5]",
     "descriptions":{
-      "en": [
-        "We can deliver it in the gauge of 1.6mm.",
-        "The available length is 38mm.",
-        "It comes with a ball size of 5mm.",
-        "This is just a cool product! Order right now!"
-      ],
-      "de": [
-        "In der Materialstärke von 1.6mm auf Lager vorrätig.",
-        "Für dich erhältlich in der Länge von 38mm.",
-        "Die Kugel dieses Piercings hat eine Größe von 5mm.",
-        "Du weißt es selbst am besten, ein tolles Produkt wie dieses fehlt deiner Sammlung noch!"
-      ]
+      "en": "We can deliver it in the gauge of 1.6mm. The available length is 38mm. It comes with a ball size of 5mm. This is just a cool product! Order right now!",
+      "de": "In der Materialstärke von 1.6mm auf Lager vorrätig. Für dich erhältlich in der Länge von 38mm. Die Kugel dieses Piercings hat eine Größe von 5mm. Du weißt es selbst am besten, ein tolles Produkt wie dieses fehlt deiner Sammlung noch!"
     },
     "properties": [
       "gauge",
@@ -27,18 +17,8 @@ var Api_response =
     "db_id": "23456",
     "propertyFormula": "+[gauge:1.6]\n+[length:38]\n+[ball:5]",
     "descriptions":{
-      "en": [
-        "The available gauge is 1.6mm.",
-        "Available with the length of 38mm.",
-        "It comes with a ball size of 5mm.",
-        "This is just a crazy product! Order right now!"
-      ],
-      "de": [
-        "In der Materialstärke von 1.6mm auf Lager vorrätig.",
-        "Groß oder klein, das entscheidest du allein – verfügbar in der Länge 38mm.",
-        "Eine 5mm große Kugel macht das gute Stück erst so richtig perfekt.",
-        "Du weißt es selbst am besten, ein wunderbares Produkt wie dieses fehlt deiner Sammlung noch!"
-      ]
+      "en": "The available gauge is 1.6mm. Available with the length of 38mm.It comes with a ball size of 5mm. This is just a crazy product! Order right now!",
+      "de": "In der Materialstärke von 1.6mm auf Lager vorrätig.Groß oder klein, das entscheidest du allein – verfügbar in der Länge 38mm. Eine 5mm große Kugel macht das gute Stück erst so richtig perfekt. Du weißt es selbst am besten, ein wunderbares Produkt wie dieses fehlt deiner Sammlung noch!"
     },
     "properties": [
       "gauge",
@@ -50,18 +30,8 @@ var Api_response =
     "db_id": "34567",
     "propertyFormula": "+[gauge:1.6]\n+[length:38]\n+[ball:5]",
     "descriptions": {
-      "en": [
-        "This article is available with the gauge of 1.6mm.",
-        "Available with the length of 38mm.",
-        "The 5mm ball is a perfect fit.",
-        "This is just a wonderful product! Order right now!"
-      ],
-      "de": [
-        "In der Materialstärke von 1.6mm auf Lager vorrätig.",
-        "Groß oder klein, das entscheidest du allein – verfügbar in der Länge 38mm.",
-        "Die Kugelgröße ist 5mm.",
-        "Du weißt es selbst am besten, ein duftes Produkt wie dieses fehlt deiner Sammlung noch!"
-      ]
+      "en": "This article is available with the gauge of 1.6mm. Available with the length of 38mm. The 5mm ball is a perfect fit. This is just a wonderful product! Order right now!",
+      "de": "In der Materialstärke von 1.6mm auf Lager vorrätig. Groß oder klein, das entscheidest du allein – verfügbar in der Länge 38mm. Die Kugelgröße ist 5mm. Du weißt es selbst am besten, ein duftes Produkt wie dieses fehlt deiner Sammlung noch!"
     },
     "properties": [
       "gauge",
@@ -73,18 +43,8 @@ var Api_response =
     "db_id": "45678",
     "propertyFormula": "+[gauge:1.6]\n+[length:38]\n+[ball:5]",
     "descriptions": {
-      "en": [
-        "The available gauge is 1.6mm.",
-        "We can deliver it in the length of 38mm.",
-        "It comes with a ball size of 5mm.",
-        "This is just a cool product! Order right now!"
-      ],
-      "de": [
-        "Ein Produkt für alle Gelegenheiten - erhältlich in der Materialstärke von 1.6mm.",
-        "Für dich erhältlich in der Länge von 38mm.",
-        "Eine 5mm große Kugel macht das gute Stück erst so richtig perfekt.",
-        "Einfach ein starkes Produkt. Am besten gleich jetzt bestellen."
-      ]
+      "en": "The available gauge is 1.6mm. We can deliver it in the length of 38mm.It comes with a ball size of 5mm. This is just a cool product! Order right now!",
+      "de": "Ein Produkt für alle Gelegenheiten - erhältlich in der Materialstärke von 1.6mm. Für dich erhältlich in der Länge von 38mm. Eine 5mm große Kugel macht das gute Stück erst so richtig perfekt. Einfach ein starkes Produkt. Am besten gleich jetzt bestellen."
     },
     "properties": [
       "gauge",
@@ -96,18 +56,8 @@ var Api_response =
     "db_id": "56789",
     "propertyFormula": "+[gauge:1.6]\n+[length:38]\n+[ball:5]",
     "descriptions": {
-      "en": [
-        "The available gauge is 1.6mm.",
-        "Available with the length of 38mm.",
-        "The 5mm ball is a perfect fit.",
-        "This is just a awesome product! Order right now!"
-      ],
-      "de": [
-        "In der Materialstärke von 1.6mm auf Lager vorrätig.",
-        "Groß oder klein, das entscheidest du allein – verfügbar in der Länge 38mm.",
-        "Die dazugehörige Kugel ist 5mm groß und leicht kugelförmig.",
-        "Worauf wartest du noch, ein spitzen Item wie das hier möchte einfach gekauft werden!"
-      ]
+      "en": "The available gauge is 1.6mm. Available with the length of 38mm. The 5mm ball is a perfect fit. This is just a awesome product! Order right now!",
+      "de": "In der Materialstärke von 1.6mm auf Lager vorrätig. Groß oder klein, das entscheidest du allein – verfügbar in der Länge 38mm. Die dazugehörige Kugel ist 5mm groß und leicht kugelförmig. Worauf wartest du noch, ein spitzen Item wie das hier möchte einfach gekauft werden!"
     },
     "properties": [
       "gauge",
