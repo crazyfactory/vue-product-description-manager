4d60fbc1c38f608d6338cbdf09cdb31c8a49d1ab
Jens Krause <jens@crazy-factory.com>
2016-11-07 16:19:42 +0700

    WIP: refactor attribute data

---
+++ config/attributes.js
@@ -0,0 +1,898 @@
+var Attributes_de =
+{
+  "id":"attributes",
+  "conjunction":{
+    "with":{
+      de:"mit",
+      en:"with"
+    },
+    "and":{
+      de:"und",
+      en:"and"
+    }
+  },
+  "content":[
+    {
+      "value":"claw-set-crystal",
+      "label":{
+      	de:"eingefasstem Kristallstein",
+      	en:"claw set crystal"
+      }
+    },
+    {
+      "value":"cone",
+      "label":{
+      	de:"Cone",
+      	en:"cone"
+      },
+    },
+    {
+      "value":"cones",
+      "label":{
+      	de:"Cones",
+      	en:"cones"
+      },
+    },
+    {
+      "value":"anodised-ball",
+      "label":{
+      	de:"farbig eloxierter Kugel",
+      	en:"anodised ball"
+      }
+    },
+    {
+      "value":"anodised-cone",
+      "label":{
+      	de:"farbig eloxiertem Cone",
+      	en:"anodised cone"
+      }
+    },
+    {
+      "value":"design_crapwaer",
+      "label":{
+      	de:"Crapwaer Design",
+      	en:"Crapwaer design"
+      }
+    },
+    {
+      "value":"design_sarah-sorceress",
+      "label":{
+      	de:"Sarah Sorceress Design",
+      	en:"Sarah Sorceress design"
+      }
+    },
+    {
+      "value":"inlay_pendant",
+      "label":{
+      	de:"Anhängerinlay",
+      	en:"pendant inlay"
+      }
+    },
+    {
+      "value":"inlay_flower",
+      "label":{
+      	de:"Blumeninlay",
+      	en:"flower inlay"
+      }
+    },
+    {
+      "value":"inlay_flower-little",
+      "label":{
+      	de:"Blümcheninlay",
+      	en:"little flower inlay"
+      }
+    },
+    {
+      "value":"inlay_cupcake",
+      "label":{
+      	de:"Cupcake-Inlay",
+      	en:"cupcake inlay"
+      }
+    },
+    {
+      "value":"inlay_cocktail",
+      "label":{
+      	de:"Cocktailinlay",
+      	en:"cocktail inlay"
+      }
+    },
+    {
+      "value":"inlay_dolphin",
+      "label":{
+      	de:"Delfininlay",
+      	en:"dolphin inlay"
+      }
+    },
+    {
+      "value":"inlay_lizard",
+      "label":{
+      	de:"Eidechseninlay",
+      	en:"lizard inlay"
+      }
+    },
+    {
+      "value":"inlay_elephant",
+      "label":{
+      	de:"Elefanteninlay",
+      	en:"elephant inlay"
+      }
+    },
+    {
+      "value":"inlay_elf",
+      "label":{
+      	de:"Elfeninlay",
+      	en:"elf inlay"
+      }
+    },
+    {
+      "value":"inlay_angel",
+      "label":{
+      	de:"Engelinlay",
+      	en:"angel inlay"
+      }
+    },
+    {
+      "value":"inlay_flip-flop",
+      "label":{
+      	de:"Flip Flop Inlay",
+      	en:"flip-flop inlay"
+      }
+    },
+    {
+      "value":"inlay_wing",
+      "label":{
+      	de:"Flügelinlay",
+      	en:"wing inlay"
+      }
+    },
+    {
+      "value":"inlay_frog",
+      "label":{
+      	de:"Froschinlay",
+      	en:"frop inlay"
+      }
+    },
+    {
+      "value":"inlay_handcuffs",
+      "label":{
+      	de:"Handschelleninlay",
+      	en:"handcuffs inlay"
+      }
+    },
+    {
+      "value":"inlay_heart",
+      "label":{
+      	de:"Herzinlay",
+      	en:"heart inlay"
+      }
+    },
+    {
+      "value":"inlay_heart-little",
+      "label":{
+      	de:"Herzcheninlay",
+      	en:"little hearts inlay"
+      }
+    },
+    {
+      "value":"inlay_horseshoe",
+      "label":{
+      	de:"Hufeiseninlay",
+      	en:"horseshoe inlay"
+      }
+    },
+    {
+      "value":"inlay_chain",
+      "label":{
+      	de:"Ketteninlay",
+      	en:"chain inlay"
+      }
+    },
+    {
+      "value":"inlay_stroller",
+      "label":{
+      	de:"Kinderwageninlay",
+      	en:"stroller inlay"
+      }
+    },
+    {
+      "value":"inlay_cherry",
+      "label":{
+      	de:"Kirschinlay",
+      	en:"cherry inlay"
+      }
+    },
+    {
+      "value":"inlay_cloverleaf",
+      "label":{
+      	de:"Kleeblattinlay",
+      	en:"cloverleaf inlay"
+      }
+    },
+    {
+      "value":"inlay_cross",
+      "label":{
+      	de:"Kreuzinlay",
+      	en:"cross inlay"
+      }
+    },
+    {
+      "value":"inlay_crown",
+      "label":{
+      	de:"Kroneninlay",
+      	en:"crown inlay"
+      }
+    },
+    {
+      "value":"inlay_dragon-fly",
+      "label":{
+      	de:"Libelleninlay",
+      	en:"dragon fly inlay"
+      }
+    },
+    {
+      "value":"inlay_little-baby",
+      "label":{
+      	de:"Little Baby Inlay",
+      	en:"little baby inlay"
+      }
+    },
+    {
+      "value":"inlay_moon",
+      "label":{
+      	de:"Mondinlay",
+      	en:"moon inlay"
+      }
+    },
+    {
+      "value":"inlay_octopus",
+      "label":{
+      	de:"Oktopusinlay",
+      	en:"octopus inlay"
+      }
+    },
+    {
+      "value":"inlay_om",
+      "label":{
+      	de:"Ominlay",
+      	en:"om inlay"
+      }
+    },
+    {
+      "value":"inlay_ace-of-spades",
+      "label":{
+      	de:"Piek Ass Inlay",
+      	en:"ace of spades inlay"
+      }
+    },
+    {
+      "value":"inlay_poodle",
+      "label":{
+      	de:"Pudelinlay",
+      	en:"poodle inlay"
+      }
+    },
+    {
+      "value":"inlay_lock",
+      "label":{
+      	de:"Schlossinlay",
+      	en:"lock inlay"
+      }
+    },
+    {
+      "value":"inlay_key",
+      "label":{
+      	de:"Schlüsselinlay",
+      	en:"lock inlay"
+      }
+    },
+    {
+      "value":"inlay_butterfly",
+      "label":{
+      	de:"Schmetterlingsinlay",
+      	en:"butterfly inlay"
+      }
+    },
+    {
+      "value":"inlay_swan",
+      "label":{
+      	de:"Schwaneninlay",
+      	en:"swan inlay"
+      }
+    },
+    {
+      "value":"inlay_spider-web",
+      "label":{
+      	de:"Spinnennetzinlay",
+      	en:"spider web inlay"
+      }
+    },
+    {
+      "value":"inlay_star",
+      "label":{
+      	de:"Sterneninlay",
+      	en:"star inlay"
+      }
+    },
+    {
+      "value":"inlay_tart",
+      "label":{
+      	de:"Törtcheninlay",
+      	en:"tart inlay"
+      }
+    },
+    {
+      "value":"inlay_skull",
+      "label":{
+      	de:"Totenschädelinlay",
+      	en:"skull inlay"
+      }
+    },
+    {
+      "value":"inlay_dice",
+      "label":{
+      	de:"Würfelinlay",
+      	en:"dice inlay"
+      }
+    },
+    {
+      "value":"jewelled",
+      "label":{
+      	de:"Kristallstein",
+      	en:"jewelled"
+      }
+    },
+    {
+      "value":"jewelled-disc",
+      "label":{
+      	de:"Kristallsteinscheibe",
+      	en:"jewelled disc"
+      }
+    },
+    {
+      "value": "pendant",
+      "label":{
+      	de:"Anhänger",
+      	en:"pendant"
+      }
+    },
+    {
+      "value": "pendant_flower",
+      "label":{
+      	de:"Blumenanhänger",
+      	en:"flower pendant"
+      }
+    },
+    {
+      "value": "pendant_flower-little",
+      "label":{
+      	de:"Blümchenanhänger",
+      	en:"little flowers pendant"
+      }
+    },
+    {
+      "value": "pendant_cupcake",
+      "label":{
+      	de:"Cupcake-Anhänger",
+      	en:"cupcake pendant"
+      }
+    },
+    {
+      "value": "pendant_cocktail",
+      "label":{
+      	de:"Cocktailanhänger",
+      	en:"cocktail pendant"
+      }
+    },
+    {
+      "value": "pendant_dolphin",
+      "label":{
+      	de:"Delfininanhänger",
+      	en:"dolphin pendant"
+      }
+    },
+    {
+      "value": "pendant_lizard",
+      "label":{
+      	de:"Eidechsenanhänger",
+      	en:"lizard pendant"
+      }
+    },
+    {
+      "value": "pendant_elephant",
+      "label":{
+      	de:"Elefantenanhänger",
+      	en:"elephant pendant"
+      }
+    },
+    {
+      "value": "pendant_elf",
+      "label":{
+      	de:"Elfenanhänger",
+      	en:"elf pendant"
+      }
+    },
+    {
+      "value": "pendant_angel",
+      "label":{
+      	de:"Engelanhänger",
+      	en:"angel pendant"
+      }
+    },
+    {
+      "value": "pendant_flip-flop",
+      "label":{
+      	de:"Flip Flop Anhänger",
+      	en:"flip-flop pendant"
+      }
+    },
+    {
+      "value": "pendant_wing",
+      "label":{
+      	de:"Flügelanhänger",
+      	en:"wing pendant"
+      }
+    },
+    {
+      "value": "pendant_frog",
+      "label":{
+      	de:"Froschanhänger",
+      	en:"frog pendant"
+      }
+    },
+    {
+      "value": "pendant_handcuffs",
+      "label":{
+      	de:"Handschellenanhänger",
+      	en:"handcuffs pendant"
+      }
+    },
+    {
+      "value": "pendant_heart",
+      "label":{
+      	de:"Herzanhänger",
+      	en:"heart pendant"
+      }
+    },
+    {
+      "value": "pendant_heart-little",
+      "label":{
+      	de:"Herzchenanhänger",
+      	en:"little hearts pendant"
+      }
+    },
+    {
+      "value": "pendant_horseshoe",
+      "label":{
+      	de:"Hufeisenanhänger",
+      	en:"horseshoe pendant"
+      }
+    },
+    {
+      "value": "pendant_chain",
+      "label":{
+      	de:"Kettenanhänger",
+      	en:"chain pendant"
+      }
+    },
+    {
+      "value": "pendant_stroller",
+      "label":{
+      	de:"Kinderwagenanhänger",
+      	en:"stroller pendant"
+      }
+    },
+    {
+      "value": "pendant_cherry",
+      "label":{
+      	de:"Kirschanhänger",
+      	en:"cherry pendant"
+      }
+    },
+    {
+      "value": "pendant_cloverleaf",
+      "label":{
+      	de:"Kleeblattanhänger",
+      	en:"cloverleaf pendant"
+      }
+    },
+    {
+      "value": "pendant_cross",
+      "label":{
+      	de:"Kreuzanhänger",
+      	en:"cross pendant"
+      }
+    },
+    {
+      "value": "pendant_crown",
+      "label":{
+      	de:"Kronenanhänger",
+      	en:"crown pendant"
+      }
+    },
+    {
+      "value": "pendant_dragon-fly",
+      "label":{
+      	de:"Libellenanhänger",
+      	en:"dragon fly pendant"
+      }
+    },
+    {
+      "value": "pendant_little-baby",
+      "label":{
+      	de:"Little Baby Anhänger",
+      	en:"little baby pendant"
+      }
+    },
+    {
+      "value": "pendant_moon",
+      "label":{
+      	de:"Mondanhänger",
+      	en:"moon pendant"
+      }
+    },
+    {
+      "value": "pendant_octopus",
+      "label":{
+      	de:"Oktopusanhänger",
+      	en:"octopus pendant"
+      }
+    },
+    {
+      "value": "pendant_om",
+      "label":{
+      	de:"Omanhänger",
+      	en:"om pendant"
+      }
+    },
+    {
+      "value": "pendant_ace-of-spades",
+      "label":{
+      	de:"Piek Ass Anhänger",
+      	en:"ace of spades pendant"
+      }
+    },
+    {
+      "value": "pendant_poodle",
+      "label":{
+      	de: "Pudelanhänger",
+      	en:"poodle pendant"
+      }
+    },
+    {
+      "value": "pendant_lock",
+      "label":{
+      	de:"",
+      	en:""
+      }// "Schlossanhänger"
+    },
+    {
+      "value": "pendant_key",
+      "label":{
+      	de:"",
+      	en:""
+      }// "Schlüsselanhänger"
+    },
+    {
+      "value": "pendant_butterfly",
+      "label":{
+      	de:"",
+      	en:""
+      }// "Schmetterlingsanhänger"
+    },
+    {
+      "value": "pendant_swan",
+      "label":{
+      	de:"",
+      	en:""
+      }// "Schwanenanhänger"
+    },
+    {
+      "value": "pendant_spider-web",
+      "label":{
+      	de:"",
+      	en:""
+      }// "Spinnennetzanhänger"
+    },
+    {
+      "value": "pendant_star",
+      "label":{
+      	de:"",
+      	en:""
+      }// "Sternenanhänger"
+    },
+    {
+      "value": "pendant_tart",
+      "label":{
+      	de:"",
+      	en:""
+      }// "Törtchenanhänger"
+    },
+    {
+      "value": "pendant_skull",
+      "label":{
+      	de:"",
+      	en:""
+      }// "Totenschädelanhänger"
+    },
+    {
+      "value": "pendant_dice",
+      "label":{
+      	de:"",
+      	en:""
+      }// "Würfelanhänger"
+    },
+    {
+      "value": "replacement-ends",
+      "label":{
+      	de:"",
+      	en:""
+      }// "Aufsatz"
+    },
+    {
+      "value":"replacement-ends_flower",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Blumenaufsatz"
+    },
+    {
+      "value":"replacement-ends_flower-little",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Blümchenaufsatz"
+    },
+    {
+      "value":"replacement-ends_cupcake",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Cupcake-Aufsatz"
+    },
+    {
+      "value":"replacement-ends_cocktail",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Cocktailaufsatz"
+    },
+    {
+      "value":"replacement-ends_dolphin",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Delfininaufsatz"
+    },
+    {
+      "value":"replacement-ends_lizard",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Eidechsenaufsatz"
+    },
+    {
+      "value":"replacement-ends_elephant",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Elefantenaufsatz"
+    },
+    {
+      "value":"replacement-ends_elf",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Elfenaufsatz"
+    },
+    {
+      "value":"replacement-ends_angel",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Engelaufsatz"
+    },
+    {
+      "value":"replacement-ends_flip-flop",
+          "label":{
+      	de:"",
+      	en:""
+      }//"Flip Flop Aufsatz"
+    },
+    {
+      "value":"replacement-ends_wing",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Flügelaufsatz"
+    },
+    {
+      "value":"replacement-ends_frog",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Froschaufsatz"
+    },
+    {
+      "value":"replacement-ends_handcuffs",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Handschellenaufsatz"
+    },
+    {
+      "value":"replacement-ends_heart",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Herzaufsatz"
+    },
+    {
+      "value":"replacement-ends_heart-little",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Herzchenaufsatz"
+    },
+    {
+      "value":"replacement-ends_horseshoe",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Hufeisenaufsatz"
+    },
+    {
+      "value":"replacement-ends_chain",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Kettenaufsatz"
+    },
+    {
+      "value":"replacement-ends_stroller",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Kinderwagenaufsatz"
+    },
+    {
+      "value":"replacement-ends_cherry",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Kirschaufsatz"
+    },
+    {
+      "value":"replacement-ends_cloverleaf",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Kleeblattaufsatz"
+    },
+    {
+      "value":"replacement-ends_cross",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Kreuzaufsatz"
+    },
+    {
+      "value":"replacement-ends_crown",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Kronenaufsatz"
+    },
+    {
+      "value":"replacement-ends_dragon-fly",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Libellenaufsatz"
+    },
+    {
+      "value":"replacement-ends_little-baby",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Little Baby Aufsatz"
+    },
+    {
+      "value":"replacement-ends_moon",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Mondaufsatz"
+    },
+    {
+      "value":"replacement-ends_octopus",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Oktopusaufsatz"
+    },
+    {
+      "value":"replacement-ends_om",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Omaufsatz"
+    },
+    {
+      "value":"replacement-ends_ace-of-spades",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Piek Ass Aufsatz"
+    },
+    {
+      "value":"replacement-ends_poodle",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Pudelaufsatz"
+    },
+    {
+      "value":"replacement-ends_lock",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Schlossaufsatz"
+    },
+    {
+      "value":"replacement-ends_key",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Schlüsselaufsatz"
+    },
+    {
+      "value":"replacement-ends_butterfly",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Schmetterlingsaufsatz"
+    },
+    {
+      "value":"replacement-ends_swan",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Schwanenaufsatz"
+    },
+    {
+      "value":"replacement-ends_spider-web",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Spinnennetzaufsatz"
+    },
+    {
+      "value":"replacement-ends_star",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Sternenaufsatz"
+    },
+    {
+      "value":"replacement-ends_tart",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Törtchenaufsatz"
+    },
+    {
+      "value":"replacement-ends_skull",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Totenschädelaufsatz"
+    },
+    {
+      "value":"replacement-ends_dice",
+      "label":{
+      	de:"",
+      	en:""
+      }//"Würfelaufsatz"
+    }
+  ]
+}
