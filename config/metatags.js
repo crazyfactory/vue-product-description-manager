f3c74499fbfd7a627349490c9d52c69d3ef44e05
Jens Krause <jens@crazy-factory.com>
2016-11-10 09:52:56 +0700

    centralize config data (attributes & metatags)

---
+++ config/metatags.js
@@ -0,0 +1,78 @@
+var MetatagsStatic ={
+    id:'metatags',
+    content:{
+        piercing:{
+            id:'piercing',
+            value:'piercing',
+            label:{
+                de:{
+                    value:'Piercing',
+                    edit:false,
+                    active:true
+                },
+                en:{
+                    value:'Piercing',
+                    edit:false,
+                    active:true
+                }
+            },
+            alias:{
+                de:{
+                    value:[
+                        "Bier-sing",
+                        "Piecing",
+                        "Körperschmuck"
+                    ],
+                    edit:false,
+                    active:true
+                },
+                en: {
+                    value: [
+                        "beer-sing",
+                        "piecing",
+                        "priecing",
+                        "peecing",
+                        "body jewellry"
+                    ],
+                    edit: false,
+                    active:true
+                }
+            }
+        },
+        mobile_case:{
+            id:'mobile_case',
+            value:'mobile_case',
+            label: {
+                de:{
+                    value:'Handyhülle',
+                    edit:false,
+                    active:true
+                },
+                en:{
+                    value:'Mobile case',
+                    edit:false,
+                    active:true
+                }
+            },
+            alias: {
+                de: {
+                    value:[
+                        "Handyhüllen",
+                        "Mobile Hülle",
+                        "Handy Hülle"
+                    ],
+                    edit:false,
+                    active:true
+                },
+                en: {
+                    value:[
+                        "mobile cases",
+                        "mobilecase"
+                    ],
+                    edit:false,
+                    active:true
+                }
+            }
+        }
+    }
+};
