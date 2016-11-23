9af41c45ba64e08697a9f12950047d39c360744a
Jens Krause <jens@crazy-factory.com>
2016-11-23 10:51:53 +0700

    add dirty state to mocked api response

--- config/api.js
+++ config/api.js
@@ -3,12 +3,14 @@ var Api_response =
   "0":{
     "db_id": "12345",
     "edit": false,
+    "dirty": false,
     "hidden":false,
     "propertyFormula": "+[gauge:1.6]\n+[length:38]\n+[ball:5]",
     "descriptions":{
       "en-GB": {
         "value":"We can deliver it in the gauge of 1.6mm. The available length is 38mm. It comes with a ball size of 5mm. This is just a cool product! Order right now!",
-        "edit":false
+        "edit": false,
+        "dirty": false
       },
       "de": {
         "value": "In der Materialstärke von 1.6mm auf Lager vorrätig. Für dich erhältlich in der Länge von 38mm. Die Kugel dieses Piercings hat eine Größe von 5mm. Du weißt es selbst am besten, ein tolles Produkt wie dieses fehlt deiner Sammlung noch!",
@@ -24,12 +26,14 @@ var Api_response =
   "1":{
     "db_id": "23456",
     "edit": false,
+    "dirty": false,
     "hidden":false,
     "propertyFormula": "+[gauge:1.6]\n+[length:38]\n+[ball:5]",
     "descriptions":{
       "en-GB": {
         "value":"The available gauge is 1.6mm. Available with the length of 38mm.It comes with a ball size of 5mm. This is just a crazy product! Order right now!",
-        "edit":false
+        "edit": false,
+        "dirty": false
       },
       "de": {
         "value":"Auf Lager in der Materialstärke von 1.6mm. Groß oder klein, das entscheidest du allein – verfügbar in der Länge 38mm. Eine 5mm große Kugel macht das gute Stück erst so richtig perfekt. Du weißt es selbst am besten, ein wunderbares Produkt wie dieses fehlt deiner Sammlung noch!",
@@ -45,6 +49,7 @@ var Api_response =
   "2":{
     "db_id": "34567",
     "edit": false,
+    "dirty": false,
     "hidden":false,
     "propertyFormula": "+[gauge:1.6]\n+[length:38]\n+[ball:5]",
     "descriptions": {
@@ -54,7 +59,8 @@ var Api_response =
       },
       "de": {
         "value":"Mit der Materialstärke von 1.6mm einfach ein Kracher. Groß oder klein, das entscheidest du allein – verfügbar in der Länge 38mm. Die Kugelgröße ist 5mm. Du weißt es selbst am besten, ein duftes Produkt wie dieses fehlt deiner Sammlung noch!",
-        "edit":false
+        "edit": false,
+        "dirty": false
       }
     },
     "properties": [
@@ -66,16 +72,19 @@ var Api_response =
   "3":{
     "db_id": "45678",
     "edit": false,
+    "dirty": false,
     "hidden":false,
     "propertyFormula": "+[gauge:1.6]\n+[length:38]\n+[ball:5]",
     "descriptions": {
       "en-GB": {
         "value":"The available gauge is 1.6mm. We can deliver it in the length of 38mm.It comes with a ball size of 5mm. This is just a cool product! Order right now!",
-        "edit":false
+        "edit": false,
+        "dirty": false
       },
       "de": {
         "value":"Ein Produkt für alle Gelegenheiten - erhältlich in der Materialstärke von 1.6mm. Für dich erhältlich in der Länge von 38mm. Eine 5mm große Kugel macht das gute Stück erst so richtig perfekt. Einfach ein starkes Produkt. Am besten gleich jetzt bestellen.",
-        "edit":false
+        "edit": false,
+        "dirty": false
       }
     },
     "properties": [
@@ -87,16 +96,19 @@ var Api_response =
   "4":{
     "db_id": "56789",
     "edit": false,
+    "dirty": false,
     "hidden":false,
     "propertyFormula": "+[gauge:1.6]\n+[length:38]\n+[ball:5]",
     "descriptions": {
       "en-GB": {
         "value":"The available gauge is 1.6mm. Available with the length of 38mm. The 5mm ball is a perfect fit. This is just a awesome product! Order right now!",
-        "edit":false
+        "edit": false,
+        "dirty": false
       },
       "de": {
         "value":"In der Materialstärke von 1.6mm auf Lager vorrätig. Groß oder klein, das entscheidest du allein – verfügbar in der Länge 38mm. Die dazugehörige Kugel ist 5mm groß und leicht kugelförmig. Worauf wartest du noch, ein spitzen Item wie das hier möchte einfach gekauft werden!",
-        "edit":false
+        "edit": false,
+        "dirty": false
       }
     },
     "properties": [
