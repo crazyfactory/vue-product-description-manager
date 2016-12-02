71550dec40472eb06d74e57389688f90dcd4f146
Jens Krause <jens@crazy-factory.com>
2016-12-02 14:56:40 +0700

    struggle for persistence

--- config/api2.js
+++ config/api2.js
@@ -70,6 +70,196 @@ var Api_response ={
         "gauge",
         "diameter"
     ]
+    },
+    "1": {
+        "id": "ABC",
+        "base_product": "black_acrylic-expander",
+        "cached_descriptions": {
+            "de": {
+                "value": "Einfach und schön: eloxierter Ball-Closure-Ring aus Chirurgenstahl in verschiedenen Farben und Größen."
+            },
+            "en-GB": {
+                "value": "Sleek and shimmery, this anodized ball closure ring made of surgical steel is available in various colours and sizes."
+            }
+        },
+        "cached_materials": {
+            "de": {
+                "value": "Chirurgenstahl 316L"
+            },
+            "en-GB": {
+                "value": "Surgical Steel 316L"
+            }
+        },
+        "cached_names": {
+            "de": {
+                "value": "Eloxierter Ball-Closure Ring"
+            },
+            "en-GB": {
+                "value": "Anodized Ball Closure Ring"
+            }
+        },
+        "cached_metatags": {
+            "de": {
+                "value": "eloxiert, Ball closure Ring, Chirurgenstahl 316L, glänzend, glatt"
+            },
+            "en-GB": {
+                "value": "tunnel, ironwood, crocodile wood, nature, alternative, pattern"
+            }
+        },
+        "component1": null,
+        "component2": null,
+        "db_id": 75,
+        "descriptions": {
+            "en": {
+                "value": "The available gauges are 1.2mm to 2.5mm. The available diameters are 8mm up to 12mm. It comes with a varity in ball sizes from 4mm to 6mm Many color choices for you: blue to yellow are ready. This is just a classy product! Order right now!",
+                "edit": false
+            },
+            "en-GB": {
+                "value": "Sleek and shimmery, this anodized ball closure ring made of surgical steel is available in various colours and sizes.",
+                "edit": false
+            },
+            "de": {
+                "value": "Einfach und schön: eloxierter Ball-Closure-Ring aus Chirurgenstahl in verschiedenen Farben und Größen.",
+                "edit": false
+            }
+        },
+        "materials": null,
+        "metatags": null,
+        "metatag_base_product": null,
+        "metatag_component1": null,
+        "metatag_component2": null,
+        "metatag_material": null,
+        "propertyFormula": "+[gauge:1.2;1.6;2.5|diameter:8;10;12|ball:4;5;6]\n-[gauge:1.2|diameter:12]\n-[gauge:1.2|ball:5;6]\n-[gauge:1.6|diameter:12]\n-[gauge:1.6|ball:5;6]\n-[gauge:2.5|diameter:8;10]\n-[gauge:2.5|ball:4;5]\n=[gauge:2.5](Preis:+1,68)\n+[color:BL;CO;GR;PU;RN;YE]",
+        "properties": [
+            "gauge",
+            "diameter",
+            "ball",
+            "color"
+        ]
+    },
+    "2":{
+    "id": "18JS-02",
+    "base_product": null,
+    "cached_descriptions": {
+    "de": {
+        "value": ""
+    },
+    "en-GB": {
+        "value": ""
+    }
+    },
+    "cached_materials": {
+    "de": {
+        "value": "Gold"
+    },
+    "en-GB": {
+        "value": "Gold"
+    }
+    },
+    "cached_names": {
+    "de": {
+        "value": "Aufsatz mit Kristallstein für Bioflex Internal Labrets"
+    },
+    "en-GB": {
+        "value": "Jeweled Stud for Bioflex Internal Labrets"
+    }
+    },
+    "cached_metatags": {
+    "de": {
+        "value": ""
+    },
+    "en-GB": {
+        "value": ""
+    }
+    },
+    "component1": null,
+    "component2": null,
+    "db_id": 4,
+    "descriptions": {
+    "en": {
+        "value": "Piercing with stones in many colors, for example aquamarine or pink. This is just a chic product! Order right now!",
+            "edit": false
+    },
+    "en-GB": {
+        "value": "Select from a wide range of stonecolors from aquamarine to pink. This is just a stylish product! Order right now!",
+            "edit": false
+    },
+    "de": {
+        "value": "Piercing mit Mode-Steinen in jeder erdenklichen Farbe (bspw. aquamarine oder rosa). Du weißt es selbst am besten, ein tipptopp Produkt wie dieses fehlt deiner Sammlung noch!",
+            "edit": false
+    }
+    },
+    "materials": null,
+    "metatags": null,
+    "metatag_base_product": null,
+    "metatag_component1": null,
+    "metatag_component2": null,
+    "metatag_material": null,
+    "propertyFormula": "+[stonecolor:AQ;CR;PI]",
+    "properties": ["stonecolor"]
+    },
+    "3": {
+    "id": "18JS-03",
+        "base_product": null,
+        "cached_descriptions": {
+        "de": {
+            "value": ""
+        },
+        "en-GB": {
+            "value": ""
+        }
+    },
+    "cached_materials": {
+        "de": {
+            "value": "Gold"
+        },
+        "en-GB": {
+            "value": "Gold"
+        }
+    },
+    "cached_names": {
+        "de": {
+            "value": "Aufsatz mit Kristallstein für Bioflex Internal Labrets"
+        },
+        "en-GB": {
+            "value": "Jeweled Stud for Bioflex Internal Labrets"
+        }
+    },
+    "cached_metatags": {
+        "de": {
+            "value": ""
+        },
+        "en-GB": {
+            "value": ""
+        }
+    },
+    "component1": null,
+        "component2": null,
+        "db_id": 5,
+        "descriptions": {
+        "en": {
+            "value": "Piercing with stones in many colors, for example aquamarine or pink. This is just a elegant product! Order right now!",
+                "edit": false
+        },
+        "en-GB": {
+            "value": "Piercing with stones in many colors, for example aquamarine or pink. This is just a awesome product! Order right now!",
+                "edit": false
+        },
+        "de": {
+            "value": "Piercing mit Mode-Steinen in jeder erdenklichen Farbe (bspw. aquamarine oder rosa). Worauf wartest du noch, ein sensationelles Item wie das hier möchte einfach gekauft werden!",
+                "edit": false
+        }
+    },
+    "materials": null,
+        "metatags": null,
+        "metatag_base_product": null,
+        "metatag_component1": null,
+        "metatag_component2": null,
+        "metatag_material": null,
+        "propertyFormula": "+[stonecolor:AQ;CR;PI]",
+        "properties": [
+        "stonecolor"
+    ]
 },
     "success": true
 }
