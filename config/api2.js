99ccdd8263dc654685c1dba290d269f65042d005
Jens Krause <jens@crazy-factory.com>
2016-12-02 14:49:52 +0700

    add secondary api mock

---
+++ config/api2.js
@@ -0,0 +1,75 @@
+var Api_response ={
+    "0": {
+    "id": "BKMCB-PIN",
+        "base_product": "ball",
+        "cached_descriptions": {
+        "de": {
+            "value": "Schlicht und schwarz. Da kann man gar nix falsch machen. Jetzt müsst ihr nur noch aussuchen, was ihr auf diesen 1.2 mm Circular Barbell aus Chirurgenstahl schraubt."
+        },
+        "en-GB": {
+            "value": "Simple. Black. No frills. Customize this 1.2 mm circular barbell with your favourite threaded accessories and you're ready to go."
+        }
+    },
+    "cached_materials": {
+        "de": {
+            "value": "Chirurgenstahl 316L"
+        },
+        "en-GB": {
+            "value": "Surgical Steel 316L"
+        }
+    },
+    "cached_names": {
+        "de": {
+            "value": "Schwarzer Circular Barbell-Stab"
+        },
+        "en-GB": {
+            "value": "Black Circular Barbell Pin"
+        }
+    },
+    "cached_metatags": {
+        "de": {
+            "value": "schwarz, Circular, Barbell-Stab, Stab, Hufeisen, Chirurgenstahl 316L, 1.2 mm, glänzend, einfach, Basic, klassisch"
+        },
+        "en-GB": {
+            "value": "black, circular, barbell-pin, pin, horseshoe, surgical steel 316L, 1.2 mm, shiny, simple, basic, classic"
+        }
+    },
+    "component1": "anodised-cone",
+        "component2": null,
+        "db_id": 540,
+        "descriptions": {
+        "en": {
+            "value": "The available gauge is 1.2mm. Get it with diameters from 6mm up to 20mm. This is just a trendy product! Order right now!",
+                "edit": false
+        },
+        "en-GB": {
+            "value": "Simple. Black. No frills. Customize this 1.2 mm circular barbell with your favourite threaded accessories and you're ready to go.",
+                "edit": false
+        },
+        "de": {
+            "value": "Schlicht und schwarz. Da kann man gar nix falsch machen. Jetzt müsst ihr nur noch aussuchen, was ihr auf diesen 1.2 mm Circular Barbell aus Chirurgenstahl schraubt.",
+                "edit": false
+        }
+    },
+    "materials": [
+        "acrylic"
+    ],
+        "metatags": null,
+        "metatag_base_product": [
+        "ball"
+    ],
+        "metatag_component1": [
+        "anodised-cone"
+    ],
+        "metatag_component2": null,
+        "metatag_material": [
+        "acrylic"
+    ],
+        "propertyFormula": "+[gauge:1.2]\n+[diameter:6;7;8;9;10;11;12;14;16;18;20]",
+        "properties": [
+        "gauge",
+        "diameter"
+    ]
+},
+    "success": true
+}
