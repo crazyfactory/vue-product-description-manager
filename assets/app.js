3d34b7858439c2e716139f8767437e316a84d8c6
Lalita <lalita@crazy-factory.co.th>
2018-01-02 13:34:58 +0700

    refactor(API communication::get_products)

--- assets/app.js
+++ assets/app.js
@@ -998,11 +998,26 @@ new Vue({
             if (hasApi) {
                 // clear product input
                 this.newProduct = ''
-
-                api.app = this
-                api.data = value
-                api.action = 'get_products'
-                api.call()
+                _this = this
+                fetch(
+                    api_endpoint,
+                    {
+                        credentials: 'include',
+                        method: 'POST',
+                        body: JSON.stringify({
+                            action: 'get_products',
+                            data: value.split(" ").join(",")
+                        })
+                    })
+                    .then(function (response) {
+                        return response.json()
+                    })
+                    .then(function (response) {
+                        _this.pushProducts(response)
+                    })
+                    .catch(function () {
+                        _this.addMessage("Sorry, something went wrong!", 'danger')
+                    })
             }
             else {
                 var my_products = value.split(" ")
@@ -1277,6 +1292,93 @@ new Vue({
                 Promise.all(promise_status_list)
                     .then(() => this.addMessage("Hey, you just updated translation status of all the " + this.resourceDict[type]['name'] + " successfully.", 'success'))
                     .catch(() => this.addMessage("Sorry, something went wrong!", 'danger'));
+            }
+        },
+        pushProducts: function(products){
+
+            var product_names = []
+
+            for (var key in products) {
+                if (key !== 'success' && key !== 'metatags' && key !== 'materials' && 'propertyFormula' in products[key]) {
+                    my_product = products[key];
+                    product_names.push(my_product.id)
+
+                    var base_product = {}
+                    if (my_product.base_product['value'] && my_product.base_product['value'] !== '-' && my_product.base_product['value'].length) {
+                        for (var i = 0; i < _this.rawBaseproducts.length; i++) {
+                            if (_this.rawBaseproducts[i]['name'] === my_product.base_product['value']) {
+                                base_product = _this.rawBaseproducts[i]
+                                break;
+                            }
+                        }
+                    }
+
+                    var component1 = {}
+                    var found_1 = false
+                    var component2 = {}
+                    var found_2 = false
+
+                    if ((my_product.component1['value'] && my_product.component1['value'] !== '-' && my_product.component1['value'].length)
+                        || (my_product.component2['value'] && my_product.component2['value'] !== '-' && my_product.component2['value'].length)) {
+
+                        for (var i = 0; i < _this.rawComponents.length; i++) {
+                            if (_this.rawComponents[i]['name'] === my_product.component1['value']) {
+                                component1 = _this.rawComponents[i]
+                                found_1 = true
+                            }
+                            if (_this.rawComponents[i]['name'] === my_product.component2['value']) {
+                                component2 = _this.rawComponents[i]
+                                found_2 = true
+                            }
+                            if (found_1 && found_2) {
+                                break
+                            }
+                        }
+                    }
+
+                    var material_stash = []
+
+                    for (var i = 0; i < products.materials.length; i++) {
+                        if (my_product.materials.indexOf(products.materials[i]['name']) > -1) {
+                            material_stash.push(products.materials[i]);
+                        }
+                    }
+
+                    var metatag_stash = []
+
+                    for (var i = 0; i < products.metatags.length; i++) {
+                        if (my_product.metatags.indexOf(products.metatags[i]['name']) > -1) {
+                            metatag_stash.push(products.metatags[i]);
+                        }
+                    }
+
+                    this.products.push({
+                        active: true,
+                        cached_descriptions: my_product.cached_descriptions,
+                        cached_materials: my_product.cached_materials,
+                        cached_metatags: my_product.cached_metatags,
+                        cached_names: my_product.cached_names,
+                        component1: component1,
+                        component2: component2,
+                        base_product: base_product,
+                        detailsLink: my_product.details_link,
+                        db_id: my_product.db_id,
+                        dirty: false,
+                        descriptions: my_product.descriptions,
+                        hidden: false,
+                        id: productStorage.uid++,
+                        materials: material_stash,
+                        metatags: metatag_stash,
+                        modelCode: my_product.id,
+                        name_scheme: null,
+                        names: {},
+                        productImage: my_product.product_image['S'],
+                        properties: my_product.properties,
+                        propertyFormula: my_product.propertyFormula,
+                        updated: Date.now()
+                    });
+
+                }
             }
         },
         productsTranslationUpdate: function(){
